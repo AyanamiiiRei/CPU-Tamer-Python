02bc1e5781e10d3feb431f42b5ca9fbb6d28be6a
skubla <real10614@gmail.com>
2026-04-02 21:49:08 +0800

    started bit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+CPU-Tamer - Python版本
+Author: Your Name
+"""
+
+def main():
+    """主函数 - 相当于C的int main()"""
+    print("Hello from Python!")
+    return 0  # 可以返回，但通常不需要
+
+def greetings():
+    print("Welcome using CPU Tamer\nThis is a version written in Python\n"
+    "Reminder: You need to run this program with root privilges!!!\n")
+
+def CLI():
+    try:
+        user_input=input("CPU-Tamer>>")
+    except EOFError: # Ctrl+D
+        user_input=None
+    except KeyboardInterrupt:# Ctrl+C
+        user_input=None
+    return user_input
+
+if __name__ == "__main__":
+    # 这是Python程序的入口点，相当于C的main()
+    greetings()
+    user_input = CLI()
+    
+    # exit(exit_code)
+    
